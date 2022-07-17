bd8d78ab5a9b7eda9d85f4054daaf7b7907d31d6
sapan <sapan@vinayak>
2022-07-17 23:58:53 +0530

    fifth commit

---
+++ Day2_Intro_To_Java/src/com/cg/java/program1.java
@@ -0,0 +1,16 @@
+package com.cg.java;
+
+import java.util.Scanner;
+
+public class program1 {
+
+	public static void main(String[] args) {
+		//int a=15;
+		Scanner s1=new Scanner(System.in);
+		double a=s1.nextDouble();
+		System.out.println("The value of a is:"+a);
+		//System.out.println("Welcome to sapana's PC");
+
+	}
+
+}
