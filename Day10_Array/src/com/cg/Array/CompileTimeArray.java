9818df00d08a2bbc69112a8c76be3714a50e1496
sapan <sapan@vinayak>
2022-07-25 10:02:43 +0530

    first commit

---
+++ Day10_Array/src/com/cg/Array/CompileTimeArray.java
@@ -0,0 +1,11 @@
+package com.cg.Array;
+
+public class CompileTimeArray {
+
+	public static void main(String[] args) {
+		int arr[]= {11,22,33,44,55};
+		System.out.println(arr[3]);
+
+		}
+
+}
---
+++ Day10_Array/src/com/cg/Array/NonPrimitiveArray.java
@@ -0,0 +1,21 @@
+package com.cg.Array;
+
+class Employee
+{
+	public int EmpId;
+	public String Name;
+	//constructor to initialize the value
+	Employee(int id,String name)
+	{
+		EmpId=id;
+		Name=name;
+	}
+}
+public class NonPrimitiveArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
